c5d2c9000c4917312a4286be6066d89410d14f9f
rodrigoTopan <rodrigo.topan.ti@gmail.com>
2019-07-06 21:22:56 -0300

    Criação de controllers File, Appointment, Provider

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import multerConfig from './config/multer'
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
- 
+import ProviderController from './app/controllers/ProviderController';
+import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth'
+import ScheduleControlller from './app/controllers/ScheduleControlller';
 
 const routes = new Router();
+const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store)
@@ -13,5 +18,14 @@ routes.post('/sessions', SessionController.store)
 routes.use(authMiddleware)
 
 routes.put('/users', UserController.update)
+
+routes.get('/providers', ProviderController.index)
+
+routes.get('/appointments', AppointmentController.index)
+routes.post('/appointments', AppointmentController.store)
+
+routes.get('/schedules',ScheduleControlller.index)
+
+routes.post('/files',upload.single('file'), FileController.store)
 
 export default routes;
